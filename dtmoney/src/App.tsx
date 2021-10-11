8aa6523f6d87ff41d2fbd46bae4caefc1e4ee67c
pedro-drosa <pedro_drosa@hotmail.com>
2021-10-11 13:59:21 -0300

    Criando estilos globais

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,13 +1,11 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <Title>Hello React</Title>
+    <div className="App">
+      <h1>Hello Word</h1>
+      <GlobalStyle />
+    </div>
   );
 }
 
