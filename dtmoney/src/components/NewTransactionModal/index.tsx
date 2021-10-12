63dbd9de10b3512ce4796b8e2c33ad00784a1587
pedro-drosa <pedro_drosa@hotmail.com>
2021-10-12 15:25:16 -0300

    Estrutura do formulário

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -12,8 +12,16 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
+      <Container>
         <h2>Cadastrar Transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria"/>
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
