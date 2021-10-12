90f25a22ec08211833adcfcc491800e609645427
pedro-drosa <pedro_drosa@hotmail.com>
2021-10-12 07:53:07 -0300

    Configurando MirageJS e Axios

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createServer } from 'miragejs';
+
 import App from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de sites',
+          amount: 3000,
+          type:'deposit',
+          category: 'Development',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
