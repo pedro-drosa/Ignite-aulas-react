c366a3747f2267dc564f793a4991ae71f30ada43
pedro-drosa <pedro_drosa@hotmail.com>
2021-11-03 08:58:50 -0300

    Title dinâmico por página

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello, <span>NextJS</span>
-    </h1>
+    <>
+      <Head>
+        <title>início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello, <span>NextJS</span>
+      </h1>
+    </>
   )
 }
