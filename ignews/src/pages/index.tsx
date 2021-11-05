c9ba16795c682ae9eb5fc3f23b23c6c84b5b08d5
pedro-drosa <pedro_drosa@hotmail.com>
2021-11-04 21:50:59 -0300

    Página Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello, Word</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖖 Hey, welcome</span>
+          <h1>News aboult the <span>React</span> world.</h1>
+          <p>
+            Get access to all publcations <br />
+            <span>form $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
