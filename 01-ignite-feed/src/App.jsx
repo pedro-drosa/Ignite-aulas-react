7d0289fdb57a19985b0da9c1012bc2d03017c87e
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-28 09:10:18 -0300

    feat(Post.tsx): create component

--- 01-ignite-feed/src/App.jsx
+++ 01-ignite-feed/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 import './global.css';
 import styles from './App.module.css';
 
@@ -10,14 +11,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <div>
-            <h1>Post 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </div>
-          <div>
-            <h1>Post 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </div>
+          <Post />
         </main>
       </div>
     </div>
---
+++ 01-ignite-feed/src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/pedro-drosa.png" />
+          <div className={styles.authorInfo}>
+            <strong>Pedro Mascarenhas</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="28 de Novembro ás 08:30h" dateTime="2022-11-28 08:30:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">pedro.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#nlw </a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
