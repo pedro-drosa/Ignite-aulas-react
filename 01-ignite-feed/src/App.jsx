e7b01d8772b95bd904a5c1fb8685bacaa963f0e4
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-28 08:08:11 -0300

    feat: add grid template

--- 01-ignite-feed/src/App.jsx
+++ 01-ignite-feed/src/App.jsx
@@ -1,8 +1,27 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import './global.css';
+import styles from './App.module.css';
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div>
+            <h1>Post 1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div>
+            <h1>Post 1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
