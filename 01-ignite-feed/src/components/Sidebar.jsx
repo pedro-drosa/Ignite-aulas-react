6557ba957323210c5967c7652210e71b0c1201c6
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-28 16:57:33 -0300

    feat(Avatar.tsx): create component

--- 01-ignite-feed/src/components/Sidebar.jsx
+++ 01-ignite-feed/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,10 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/pedro-drosa.png"
-        />
+        <Avatar src="https://github.com/pedro-drosa.png" />
         <strong>Pedro Mascarenhas</strong>
         <span>Web Developer</span>
       </div>
