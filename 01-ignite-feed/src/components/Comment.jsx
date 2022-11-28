812ce971572e0c7b52bbe24d8ee3b39baef5d076
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-28 11:42:23 -0300

    feat(Comment.tsx): add component

---
+++ 01-ignite-feed/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/pedro-drosa.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Mascarenhas</strong>
+              <time
+                title="28 de Novembro ás 08:30h"
+                dateTime="2022-11-28 08:30:00"
+              >
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Pedro, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
