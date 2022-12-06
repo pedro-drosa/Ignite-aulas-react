c48364e3cf6df7d7bf33bf5bd9f567c99416afb0
pedro-drosa <pedro_drosa@hotmail.com>
2022-12-06 17:43:50 -0300

    feat: custom validation

--- 01-ignite-feed/src/components/Post.jsx
+++ 01-ignite-feed/src/components/Post.jsx
@@ -21,7 +21,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
@@ -30,6 +35,8 @@ export function Post({ author, publishedAt, content }) {
     );
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.trim().length === 0;
 
   return (
     <article className={styles.post}>
@@ -73,9 +80,13 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          required
+          onInvalid={handleNewCommentInvalid}
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
